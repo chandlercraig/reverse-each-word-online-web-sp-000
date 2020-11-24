0a1b55e079e257eac8d662a632443cbb7b8cdb70
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 19:02:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_arr = string.split
   new_new_arr = []
-  new_arr.each do |word|
+  new_arr.collect do |word|
     new_new_arr << word.reverse
   end
   new_new_arr.join(" ")
